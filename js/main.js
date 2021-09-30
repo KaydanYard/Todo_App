78e4606443c26c91cd6675c4f212d39e13539c1d
Kaydan Yardley <kaydan@Kaydans-MacBook-Pro.local>
2021-09-30 08:58:12 -0600

    working on render function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,121 @@
 const lists = {
-  1: {name: 'Shopping list'},
-  2: {name: 'Honey do list'},
-  3: {name: ''},
-  4: {name: ''},
-  5: {name: ''},
-  6: {name: ''},
-  7: {name: ''},
-  8: {name: ''},
-  9: {name: ''},
-};
+  1: {
+    name: 'Shopping list',
+    todos: [
+      {
+        text: 'bananas',
+        completed: false
+      },
+      {
+        text: '1 lbs ground turkey',
+        completed: false
+      }
+    ]
+  },
+  2: {
+    name: 'Honey do list',
+    todos: [
+      {
+        text: 'bananas',
+        completed: false
+      },
+      {
+        text: '1 lbs ground turkey',
+        completed: false
+      }
+    ]
+  },
+  3: {
+    name: '',
+    todos: [
+      {
+        text: '',
+        completed: false
+      },
+      {
+        text: '',
+        completed: false
+      }
+    ]
+  },
+  4: {
+    name: '',
+    todos: [
+      {
+        text: '',
+        completed: false
+      },
+      {
+        text: '',
+        completed: false
+      }
+    ]
+  },
+  5: {    
+    name: '',
+    todos: [
+      {
+        text: '',
+        completed: false
+      },
+      {
+        text: '',
+        completed: false
+      }
+    ]
+  },
+  6: {
+    name: '',
+    todos: [
+      {
+        text: '',
+        completed: false
+      },
+      {
+        text: '',
+        completed: false
+      }
+    ]
+  },
+  7: {
+    name: '',
+    todos: [
+      {
+        text: '',
+        completed: false
+      },
+      {
+        text: '',
+        completed: false
+      }
+    ]
+  },
+  8: {
+    name: '',
+    todos: [
+      {
+        text: '',
+        completed: false
+      },
+      {
+        text: '',
+        completed: false
+      }
+    ]
+  },
+  9: {
+    name: '',
+    todos: [
+      {
+        text: '',
+        completed: false
+      },
+      {
+        text: '',
+        completed: false
+      }
+    ]
+  },
+}
+
+const currentList = lists[0];
